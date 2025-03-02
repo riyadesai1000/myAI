85d7ec23ff55871632dee4a926b6dbafff7823b7
riyadesai1000 <riyadesai1000@gmail.com>
2025-03-02 15:39:47 -0500

    Update chat.ts
    Updated the introduction of the AI.

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,7 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
-export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having coming up with a response. Please try again later.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I'm here to assist you with selecting the perfect pair of Warby Parker glasses. I can help you find styles that suit your taste, explain different lens options, or answer any questions about Warby Parker in general!`;
+export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble coming up with a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
-export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+export const HISTORY_CONTEXT_LENGTH: number = 10; // Number of messages to use for context when generating a response
