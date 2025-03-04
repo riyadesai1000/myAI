5ceee73c7df098094fe1b652ef69e7b10aba20fe
riyadesai1000 <riyadesai1000@gmail.com>
2025-03-04 17:26:47 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import ChatInput from "@/components/chat/input";
 import ChatMessages from "@/components/chat/messages";
 import useApp from "@/hooks/use-app";
 import ChatHeader from "@/components/chat/header";
-import DarkModeToggle from "@/components/DarkModeToggle";
 
 export default function Chat() {
   const {
@@ -18,13 +17,12 @@ export default function Chat() {
   } = useApp();
 
   return (
-    <div className="min-h-screen w-full bg-gradient-to-b from-blue-50 via-blue-300 to-blue-50 text-gray-900 dark:bg-gradient-to-b dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 dark:text-gray-100">
+    <>
       <ChatHeader clearMessages={clearMessages} />
-      <div className="flex justify-end w-full p-4">
-        <DarkModeToggle />
-      </div>
-      <div className="flex flex-col justify-center items-center min-h-screen pt-16 px-5">
-        <ChatMessages messages={messages} indicatorState={indicatorState} />
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col max-w-screen-lg w-full h-full p-5">
+          <ChatMessages messages={messages} indicatorState={indicatorState} />
+        </div>
       </div>
       <ChatInput
         handleInputChange={handleInputChange}
@@ -32,6 +30,7 @@ export default function Chat() {
         input={input}
         isLoading={isLoading}
       />
-    </div>
+    </>
   );
 }
+
