1b77e4c1878f746724f60f35c38c519446274ff4
riyadesai1000 <riyadesai1000@gmail.com>
2025-03-05 00:03:43 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -4,4 +4,4 @@ export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s A
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble coming up with a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
-export const HISTORY_CONTEXT_LENGTH: number = 10; // Number of messages to use for context when generating a response
+export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
