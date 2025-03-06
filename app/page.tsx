99b8e69f615acded95698a3f756e220a82f2c9ba
riyadesai1000 <riyadesai1000@gmail.com>
2025-03-06 12:04:35 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import ChatInput from "@/components/chat/input";
 import ChatMessages from "@/components/chat/messages";
 import useApp from "@/hooks/use-app";
 import ChatHeader from "@/components/chat/header";
-import ShoppingCart from "@/components/ShoppingCart";
-import { useState } from "react";
+import ShoppingCart from "@/components/chat/ShoppingCart";
 
 export default function Chat() {
   const {
@@ -18,16 +18,17 @@ export default function Chat() {
     clearMessages,
   } = useApp();
 
-  // Manage Shopping Cart visibility
+  // State for controlling cart visibility
   const [showCart, setShowCart] = useState(false);
   const toggleCart = () => setShowCart((prev) => !prev);
 
   return (
     <div className="min-h-screen w-full bg-gradient-to-b from-blue-50 via-blue-300 to-blue-50 text-gray-900 dark:bg-gradient-to-b dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 dark:text-gray-100">
+      {/* Pass the toggleCart function as a prop */}
       <ChatHeader clearMessages={clearMessages} toggleCart={toggleCart} />
       <div className="flex flex-col justify-center items-center min-h-screen pt-16 px-5 space-y-8">
-        {showCart && <ShoppingCart />}
         <ChatMessages messages={messages} indicatorState={indicatorState} />
+        {showCart && <ShoppingCart />}
       </div>
       <ChatInput
         handleInputChange={handleInputChange}
@@ -38,6 +39,7 @@ export default function Chat() {
     </div>
   );
 }
+
 
 
 
