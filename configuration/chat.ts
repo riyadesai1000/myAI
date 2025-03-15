119b65215891838f27d1be2ea2660078671f3cf2
riyadesai1000 <riyadesai1000@gmail.com>
2025-03-15 13:10:40 -0700

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I'm here to assist you with selecting the perfect pair of Warby Parker glasses. I can help you show what different frames look like or even compare them using the Compare Frames in the top-right corner. Or, I can answer any questions about Warby Parker in general!`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. I'm here to help you find your ideal pair of Warby Parker glasses. I can display various frame styles, let you compare frames using the "Compare Frames" option in the top-right corner, or answer any general questions you might have about Warby Parker.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble coming up with a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
